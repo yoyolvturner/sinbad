8cbff41926d3d536dad53e4fee84bdccde5eed20
Lee Turner <lvturner@gmail.com>
2011-08-28 12:19:25 +0100

    should now exit cleanly

--- lib/sinbad/generator/application.rb
+++ lib/sinbad/generator/application.rb
@@ -7,34 +7,37 @@ class Sinbad
       class << self
 
         def run!(*args)
-        OptionParser.new do |options|
-          options.banner = "Usage: #{File.basename($0)} [path]"
+          OptionParser.new do |options|
+            options.banner = "Usage: #{File.basename($0)} [path]"
 
-          options.on("-h", "--help", "Displays this help info") do
-            puts options
-            exit 0
+            options.on("-h", "--help", "Displays this help info") do
+              puts options
+              exit 0
+            end
+
+            begin
+              options.parse!(args)
+            rescue OptionParser::ParseError => e
+              warn e.message
+              puts options
+              exit 1
+            end
+
+            # Quick ARGV check
+            if args.empty?
+              abort "No directory specified!"
+            elsif File.exists?(args.first)
+              abort "Directory already exists"
+            elsif args.length > 1
+              abort "I don't think you meant to do that..."
+            end
+
+            create_directories(args.first)
           end
 
-          begin
-            options.parse!(args)
-          rescue OptionParser::ParseError => e
-            warn e.message
-            puts options
-            exit 1
-          end
-
-          # Quick ARGV check
-          if args.empty?
-            abort "No directory specified!"
-          elsif File.exists?(args.first)
-            abort "Directory already exists"
-          elsif args.length > 1
-            abort "I don't think you meant to do that..."
-          end
-
-          create_directories(args.first)
+          return 0
         end
-        end
+
         def create_directories(root_directory)
           Dir::mkdir(root_directory)
           puts "Created project \"#{root_directory}\"\n"
